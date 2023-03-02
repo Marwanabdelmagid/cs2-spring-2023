8a5298be03bf6e40c2447ee482363d2b3912ad57
Marwanabdelmagid <98695921+Marwanabdelmagid@users.noreply.github.com>
2023-03-02 21:48:03 +0200

    Update clusters.h

--- clusters.h
+++ clusters.h
@@ -3,39 +3,47 @@ using namespace std;
 
 class point {
 private:
-	float x, y;
+    float x, y;
 public:
-	point(){
-		x = 0;
-		y = 0;
-	}
-	point(float x1, float y1){
-		x = x1;
-		y = y1;
-	}
-	void set_x(float x1) {
-		x = x1;
-	}
-	float get_x() {
-		return x;
-	}
-	void set_y(float y1) {
-		y = y1;
-	}
-	float get_y() {
-		return y;
-	}
-	void displaycord() {
-		cout << "(" << x << "," << y << ")" << endl;
+    //coonstructor
+    point(){
+        set_x(0);
+        set_y(0);
+    }
+    //paramtriezed constructor
+    point(float x1, float y1){
+        set_x(x1);
+        set_y(y1);
+    }
+    //setter
+    void set_x(float x1) {
+        x = x1;
+    }
+    //getter
+    float get_x() {
+        return x;
+    }
+    //setter
+    void set_y(float y1) {
+        y = y1;
+    }
+    float get_y() {
+        return y;
+    }
+    //getter
+    void displaycord() {
+        cout << "(" << x << "," << y << ")" << endl;
 
-	}
-	void displaycord(float x, float y) {
-		cout << "(" << x << "," << y << ")" << endl;
+    }
+    //display corrodinates
+    void displaycord(float x1, float y1) {
+        cout << "(" << x1 << "," << y1 << ")" << endl;
 
-	}
-	float disttop(float x2, float y2) {
-		float dist;
-		dist = powf((x - x2), 2) + powf((y - y2), 2);
-		return powf(dist, 0.5);
-	}
+    }
+    //function used to get distance between point p and another point 
+    float disttop(float x2, float y2) {
+        float dist;
+        dist = powf((x - x2), 2) + powf((y - y2), 2);
+        return powf(dist, 0.5);
+    }
 };
